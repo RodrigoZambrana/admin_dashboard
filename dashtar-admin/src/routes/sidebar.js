36086e2cc7d8da369ca514193187eee9421450ad
RodrigoZambrana <rodrigo@lacnic.net>
2022-07-26 15:51:53 -0300

    primer push. Funciona front y backend

---
+++ dashtar-admin/src/routes/sidebar.js
@@ -0,0 +1,61 @@
+import {
+  FiGrid,
+  FiShoppingBag,
+  FiUsers,
+  FiUser,
+  FiCompass,
+  FiGift,
+  FiList,
+  FiSettings,
+} from "react-icons/fi";
+/**
+ * ⚠ These are used just to render the Sidebar!
+ * You can include any link here, local or external.
+ *
+ * If you're looking to actual Router routes, go to
+ * `routes/index.js`
+ */
+const sidebar = [
+  {
+    path: "/dashboard", // the url
+    icon: FiGrid, // icon
+    name: "Estadisticas", // name that appear in Sidebar
+  },
+  {
+    path: "/products",
+    icon: FiShoppingBag,
+    name: "Productos",
+  },
+  {
+    path: "/category",
+    icon: FiList,
+    name: "Categorias",
+  },
+  {
+    path: "/customers",
+    icon: FiUsers,
+    name: "Clientes",
+  },
+  {
+    path: "/orders",
+    icon: FiCompass,
+    name: "Pedidos",
+  },
+  {
+    path: "/coupons",
+    icon: FiGift,
+    name: "Coupones",
+  },
+  {
+    path: "/our-staff",
+    icon: FiUser,
+    name: "Presupuestos",
+  },
+  {
+    path: "/setting",
+    icon: FiSettings,
+    name: "Facturas",
+  },
+];
+
+export default sidebar;
